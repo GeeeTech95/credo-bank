6164ace16982b0c6485a5967c5a75a2a25cf5fc5
kyle_coder <geeetech.inc@gmail.com>
2021-07-06 12:59:08 -0700

    corrected grammatical errors

--- company/urls.py
+++ company/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('services/',Services.as_view(),name = 'info'),
     path('FAQ/',Faq.as_view(),name='faq'),
     #path('terms-of-service/',TOS.as_view(),name = 'tos'),  
-    path('loan-and-investment/',LoanView.as_view(),name = 'loan&investment'),
+    #path('loan-and-investment/',LoanView.as_view(),name = 'loan&investment'),
     path('services/',Services.as_view(),name='services')  
     
 ]
