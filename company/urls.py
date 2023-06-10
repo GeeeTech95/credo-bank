ccb1cd1922adb59e37d4a17cdcf9676f39493c88
unknown <aguikenna@fiberswift.com>
2023-06-10 17:23:28 +0100

    ''

--- company/urls.py
+++ company/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path,include
-from .pages import Index,Services,TOS,About,Contact,Faq,LoanView
-from .services import Services
+from .pages import Index,About,ModernBanking,Contact
+
 
 
 urlpatterns = [
     path('',Index.as_view(),name = 'index'),
-    path('test/',About.as_view(),name = 'about-us'),
-    path('services/',Services.as_view(),name = 'info'),
-    path('FAQ/',Faq.as_view(),name='faq'),
-    #path('terms-of-service/',TOS.as_view(),name = 'tos'),  
-    #path('loan-and-investment/',LoanView.as_view(),name = 'loan&investment'),
-    path('services/',Services.as_view(),name='services')  
+    path('about-us/',About.as_view(),name='about') ,
+    path('contact-us/',Contact.as_view(),name='contact') ,
+    path('careers/',About.as_view(),name='careers')  ,
+    path('modern-banking/',ModernBanking.as_view(),name='modern-banking') ,
     
 ]
