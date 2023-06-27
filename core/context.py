3f6d36bbd7e8b714e915aa31694228f6a7cf2c49
unknown <aguikenna@fiberswift.com>
2023-06-27 13:57:59 +0100

    ''

--- core/context.py
+++ core/context.py
@@ -3,6 +3,9 @@
 def core(request) :
     context = {
         'site_name' : "Credo Capital Bank",
-        'site_name_short' : "Credo Capital"
+        'site_name_short' : "Credo Capital",
+        "site_address" : "Russell St, Abbotsford VIC 3067, Australia",
+        "site_mail" :  "support@credocapitalbank.com"
+         
     }
     return context
